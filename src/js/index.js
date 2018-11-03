b58138e22ad3febf01b573023a05a9fe8d8d7f5e
ManzDev <JoseRomanHernandez@gmail.com>
2018-11-03 19:01:35 +0000

    E-moji easy web component

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
-/*** index.js ***/
+class Emoji extends HTMLElement {
+  constructor() {
+    super();
+    const name = this.getAttribute('name');
+    const size = this.getAttribute('size') || 16;
+    const map = {
+      happy: '😄',
+      sad: '😞',
+      angry: '😠',
+      sunglasses: '😎',
+      laugh: '🤣',
+      wink: '😉',
+      love: '😍',
+      money: '🤑'
+    };
+    this.textContent = (name) ? map[name] : '😶';
+    this.style.fontSize = `${size}px`;
+  }
+}
+
+customElements.define('e-moji', Emoji);
